1867da74ce1deed2507fba39f2f7d2bb47bfbdbe
Abhijit <abhijitshendagescoe@gmail.com>
2024-08-03 03:17:23 +0530

    Periodic Service with Database Environment
    Database environment updgrade and the periodic microservice creation using docker

--- periodic/database/db.py
+++ periodic/database/db.py
@@ -1,11 +1,18 @@
 import logging
+import os
 from pymongo import MongoClient
 
 # Set debug mode globally
 logging.basicConfig(level=logging.DEBUG)
 
-url = 'mongodb://localhost:27017/'
-dbName = 'CorkDashboard'
+# Retrieve MongoDB configuration from environment variables
+MONGODB_URL = os.getenv('MONGODB_URL', 'mongodb://localhost:27017/')
+DB_NAME = os.getenv('DB_NAME', 'CorkDashboard')
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+
+if DB_USERNAME and DB_PASSWORD:
+    MONGODB_URL = f'mongodb://{DB_USERNAME}:{DB_PASSWORD}@{MONGODB_URL.split("//")[1]}'
 
 db = None  # Hold the database connection
 
@@ -14,8 +21,8 @@ def connect_to_database():
     if db:
         return db  # If db connection is already established, return it
     try:
-        client = MongoClient(url)
-        db = client[dbName]  # Get the database instance from the client
+        client = MongoClient(MONGODB_URL)
+        db = client[DB_NAME]  # Get the database instance from the client
         logging.debug('Connected to MongoDB')
         return db  # Return the database instance
     except Exception as error:
